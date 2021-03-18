ea668decf420e4bd41a2bb16d149d6650be2a9aa
IdealJakob <58514314+IdealJakob@users.noreply.github.com>
2021-03-18 07:54:44 +0100

    Update YouTube_Downloader.py

--- YouTube_Downloader.py
+++ YouTube_Downloader.py
@@ -1,12 +1,18 @@
 import pytube
 from pytube import YouTube
+print('You have to restart everytime you want to download a Video!')
 
+# Get Video URL and get Video from YouTube
 video_url = input('Video URL: ')
 youtube = pytube.YouTube(video_url)
+# selects highest resolution of the video to download
 video = youtube.streams.get_highest_resolution()
+#download video
 video.download()
 print('Download Finished')
+print('You will find the downloaded in the same folder as the .exe or .py')
 
+# script to stop the programm
 determinate = input('Press any key to terminate the program')
 terminate = 'Bye Bye'
 print(terminate)
