725dfa189e02b1773d96201ccbb45555e33bda14
IdealJakob <58514314+IdealJakob@users.noreply.github.com>
2021-05-05 19:29:46 +0200

    Updated  YoutTube_Downloader.py and .exe

--- YouTube_Downloader.py
+++ YouTube_Downloader.py
@@ -8,8 +8,13 @@ video_url = input('Video URL: ')
 youtube = pytube.YouTube(video_url)
 # selects highest resolution of the video to download
 video = youtube.streams.get_highest_resolution()
+# get path where to download this video to
+print('Please write the Path to the Folder where you want this file in the next field!')
+print('If you want it in the folder where this python file is leave blank.')
+print('Paste from your clipboard with right click!')
+path = input('Insert Folder Path here: ')
 # Download video
-video.download()
+video.download(path)
 print('Download Finished')
 print('You will find the downloaded Video in the same folder as the .exe or .py')
 
